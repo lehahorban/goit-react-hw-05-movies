8fb8d4abb828e2fc903df1aa68404af8e68bfbe0
lehahorban <94637142+lehahorban@users.noreply.github.com>
2022-08-26 17:12:36 +0000

    Deploying to gh-pages from @ lehahorban/goit-react-hw-05-movies@27b11e8836cd8f977e2af6764c01d44f69842d24 🚀

---
+++ static/js/38.be47c09a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[38],{38:function(e,n,t){t.r(n);var i=t(501),r=t(184);n.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h2",{children:"Page not found"}),(0,r.jsx)(i.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=38.be47c09a.chunk.js.map
